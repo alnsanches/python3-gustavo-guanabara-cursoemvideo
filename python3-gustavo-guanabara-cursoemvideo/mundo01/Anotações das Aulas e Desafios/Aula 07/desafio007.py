507028f755f9bfc69d1140a8abc7ee205cf4e5ef
Aline Sanches <96800792+alnsanches@users.noreply.github.com>
2025-04-22 14:43:26 -0300

    Delete python3-gustavo-guanabara-cursoemvideo/mundo01/Anotações das Aulas e Desafios directory

--- python3-gustavo-guanabara-cursoemvideo/mundo01/Anotações das Aulas e Desafios/Aula 07/desafio007.py
+++
@@ -1,8 +0,0 @@
-#DESAFIO 007: Média Aritmética
-
-# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
-
-nota1 = float(input('Digite a primeira nota: '))
-nota2 = float(input('Digite a segunda nota: '))
-media = (nota1 + nota2) / 2
-print('A primeira nota foi {}, a segunda nota foi {} e a média entre elas é {}.'.format(nota1, nota2, media))
