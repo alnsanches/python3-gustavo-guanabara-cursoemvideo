6b506408b5933fc7b9275d11b1d1c00a1e381ac6
Aline Sanches <96800792+alnsanches@users.noreply.github.com>
2025-04-22 14:51:09 -0300

    Delete Mundo 01/Anotações das Aulas e Desafios directory

--- Mundo 01/Anotações das Aulas e Desafios/Aula 06/desafio003_1.py
+++
@@ -1,9 +0,0 @@
-#DESAFIO 003.2: Somando Dois Números
-
-#Crie um programa que leia dois números e mostre a soma entre eles.
-
-
-n1 = int(input('Digite o primeiro número: '))
-n2 = int(input('Digite o segundo número: '))
-soma = n1 + n2
-print('A soma entre {} e {} vale {}!'.format(n1, n2, soma))
